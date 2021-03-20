06a4f7bcaa62756de849a67ec8e9764c90945c30
Taichi FURUHASHI <mapconcierge@gmail.com>
2021-03-20 22:56:20 +0900

    あかずきん 動画追加

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6b492f4528595f61cfd7.js"
+    "url": "webpack-runtime-96664e12b5f177975bfa.js"
   },
   {
     "url": "framework-63ec46a3540d83159f28.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8735428ecc7eba00d05ca51851648d9d"
+    "revision": "c3a7748191d31f87a3028788211fbd54"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
